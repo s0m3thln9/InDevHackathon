57d25c7be3dc66443f8db4c05f5caee719f448b7
s0m3thlin9 <dunlock67@gmail.com>
2025-05-16 18:58:36 +0300

    add base login page and add test axios request

--- app/src/app/App.tsx
+++ app/src/app/App.tsx
@@ -1,9 +1,16 @@
+import { Login } from "@pages/login"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { Home } from "@pages/home"
 import { Register } from "@pages/register"
 function App() {
   return (
-    <>
-      <Register />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
   )
 }
 
